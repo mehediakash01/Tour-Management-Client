8fb83227abac2c633c47973efd8cde760ecbf6b3
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-08-09 04:14:48 +0600

    added some animation in card

--- src/Components/FeaturedSection/Featured.jsx
+++ src/Components/FeaturedSection/Featured.jsx
@@ -1,50 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from "react";
+import { Link } from "react-router";
 import { IoLocationOutline } from "react-icons/io5";
-import { FaArrowRight } from 'react-icons/fa6';
+import { FaArrowRight } from "react-icons/fa6";
 import { MdGroups2 } from "react-icons/md";
-const Featured = ({featuredData}) => {
-     const {
+import { motion } from "framer-motion";
+const Featured = ({ featuredData }) => {
+  const {
     duration,
     _id,
     image,
     tour_name,
-bookingCount,
+    bookingCount,
 
-   package_details
-  
+    package_details,
   } = featuredData;
-console.log(featuredData);
+  console.log(featuredData);
 
-
-    return (
-   <div className="card bg-base-100  shadow-sm ">
-      <figure className='relative'>
-        <img src={image} className="h-48 w-full  object-cover" alt="tourImage" />
-           <p className="font-semibold text-sm absolute bg-primary rounded-r-full p-1 text-white -mt-20 -ml-64">
-             
-     🗓{duration}
+  return (
+    <div className="card bg-base-100  shadow-sm ">
+      <figure className="relative">
+        <motion.img
+          whileHover={{ scale: 1.2 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+          src={image}
+          className="h-48 w-full object-cover"
+          alt="tourImage"
+        />
+        <p className="font-semibold text-sm absolute bg-primary rounded-r-full p-1 text-white -mt-20 -ml-64">
+          🗓{duration}
         </p>
-          <h2 className=" absolute backdrop-blur-2xl -mb-40 py-2 w-full  text-white flex items-center justify-center gap-2 "><IoLocationOutline />{tour_name}</h2>
+        <h2 className=" absolute backdrop-blur-2xl -mb-40 py-2 w-full  text-white flex items-center justify-center gap-2 ">
+          <IoLocationOutline />
+          {tour_name}
+        </h2>
       </figure>
       <div className="card-body space-y-1 ">
-     
+        <p>{package_details.slice(0, 70)}...</p>
 
-    
-     
-      
-        <p>{ package_details.slice(0,70)}...</p>
-      
-        <div className='flex gap-3 '>
+        <div className="flex gap-3 ">
           <Link to={`/package/${_id}`}>
-            <button className="btn btn-outline btn-secondary flex items-center">view details <FaArrowRight /></button>
-          
+            <button className="btn btn-outline btn-secondary flex items-center">
+              view details <FaArrowRight />
+            </button>
           </Link>
-            <p className='flex items-center bg-secondary text-white rounded-md justify-center gap-1'> <MdGroups2 />{ bookingCount} Booked</p>
+          <p className="flex items-center bg-secondary hover:scale-110 transition-transform duration-300 text-white rounded-md justify-center gap-1">
+            {" "}
+            <MdGroups2 />
+            {bookingCount} Booked
+          </p>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Featured;
